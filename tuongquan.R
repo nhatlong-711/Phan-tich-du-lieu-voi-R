cb02794babde1ccf4f0577d7e005967a11744d3d
Hoang Phuc Nhat Long <115168657+nhatlong-711@users.noreply.github.com>
2024-01-07 00:32:09 +0700

    Add files via upload

---
+++ tuongquan.R
@@ -0,0 +1,18 @@
+# Step 1: Import package
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+library(corrplot)
+
+data <- read.csv("C:/Users/ACER/Desktop/STUDY/R_lp/R_analyst/stockx.csv", header=TRUE)
+data
+
+print(head(data, 10))
+
+# Step 3: Draw Heatmap chart
+numeric_columns <- df %>% select_if(is.numeric) %>% colnames()
+correlation_matrix <- cor(df[numeric_columns])
+
+options(repr.plot.width=20, repr.plot.height=9)  
+corrplot(correlation_matrix, method="color", col=colorRampPalette(c("navy", "white", "darkred"))(20), addCoef.col="black", number.cex=0.7, tl.col="black")
+title("Biểu đồ tương quan giữa các biến", cex.main=2, font.main=2)
